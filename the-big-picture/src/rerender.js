2687ccb04b35fa38526aff6a19a3988acba855ea
Griffin <Griffin04901@gmail.com>
2021-07-31 11:52:34 -0500

    add edit/view mode for a movie

--- the-big-picture/src/rerender.js
+++ the-big-picture/src/rerender.js
@@ -5,6 +5,11 @@ const backButton = document.getElementById('back');
 const addMovieButton = document.getElementById('add-a-movie-button');
 const findAddTitle = document.getElementById('find/add-title');
 const findAddbutton = document.getElementById('find/add-button');
+const movieBack = document.getElementById('movie-back');
+const edit = document.getElementById('edit');
+const movieView = document.getElementById('movie-view');
+const editableItems = document.getElementsByClassName('edit-item');
+const saveButton = document.getElementById('save');
 
 findMovieButton.addEventListener('click', e => {
     mainMenuDiv.classList.add('hide');
@@ -14,6 +19,11 @@ findMovieButton.addEventListener('click', e => {
   });
 
 backButton.addEventListener('click', e => {
+    movieView.classList.add('hide');
+    mainMenuDiv.classList.remove('hide');
+})
+
+movieBack.addEventListener('click', e => {
     findMovieDiv.classList.add('hide');
     mainMenuDiv.classList.remove('hide');
 })
@@ -23,4 +33,22 @@ addMovieButton.addEventListener('click', e => {
     findAddTitle.innerText = "Add a Movie";
     findAddbutton.innerText = "Add";
     findMovieDiv.classList.remove('hide');
+})
+
+edit.addEventListener('click', e => {
+    for (let i = 0; i < editableItems.length; i++) {
+        console.log(editableItems[i])
+        editableItems[i].classList.remove('hide')
+    }
+    saveButton.classList.remove('hide');
+    edit.classList.add('hide');
+})
+
+saveButton.addEventListener('click', e => {
+    for (let i = 0; i < editableItems.length; i++) {
+        console.log(editableItems[i])
+        editableItems[i].classList.add('hide')
+    }
+    saveButton.classList.add('hide')
+    edit.classList.remove('hide')
 })
