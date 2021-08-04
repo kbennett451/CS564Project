319599407cf32f4219acd75da162e99e835c203b
Griffin <Griffin04901@gmail.com>
2021-08-03 21:30:30 -0500

    Major UI updates, db creation updates

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Link to Project Plan: https://docs.google.com/document/d/17wSaF4pn6qXrcXjzEt3dyd
 
 Link to Checkpoint 2: https://docs.google.com/document/d/1ksj3vkYeomRzGj1DVNdUeyXJaO1msMz0ActlyChqmEQ/edit?usp=sharing
 
+When creating the database, ensure that rotten_tomatoes_critic_reviews.csv is downloaded. It can be obtained from https://www.kaggle.com/stefanoleone992/rotten-tomatoes-movies-and-critic-reviews-dataset?select=rotten_tomatoes_critic_reviews.csv
+
+    From there you should be able to run: sqlite3 theBigPicture.db ".read ../createDB.txt"
--- the-big-picture/src/rerender.js
+++ the-big-picture/src/rerender.js
@@ -1,54 +1,204 @@
-const findMovieButton = document.getElementById('find-a-movie-button');
-const findMovieDiv = document.getElementsByClassName('find-a-movie')[0];
+/* Main Menu View */
 const mainMenuDiv = document.getElementsByClassName('main-menu')[0];
-const backButton = document.getElementById('back');
+const findMovieButton = document.getElementById('find-a-movie-button');
 const addMovieButton = document.getElementById('add-a-movie-button');
+
+/* Find / Add View */
+const findMovieDiv = document.getElementsByClassName('find-a-movie')[0];
 const findAddTitle = document.getElementById('find/add-title');
 const findAddbutton = document.getElementById('find/add-button');
-const movieBack = document.getElementById('movie-back');
-const edit = document.getElementById('edit');
+
+/* View Movie Content */
 const movieView = document.getElementById('movie-view');
+const movieBack = document.getElementById('movie-back');
+
+/* Edit Functionality */
+const edit = document.getElementById('edit');
 const editableItems = document.getElementsByClassName('edit-item');
 const saveButton = document.getElementById('save');
 
-findMovieButton.addEventListener('click', e => {
-    mainMenuDiv.classList.add('hide');
-    findAddTitle.innerText = "Find a Movie";
-    findAddbutton.innerText = "Find";
-    findMovieDiv.classList.remove('hide');
-  });
+/* Overlay Specific Code */
+const shield = document.getElementById('shield');
 
-backButton.addEventListener('click', e => {
+/* Shared */
+const backButton = document.getElementById('back');
+
+backButton.addEventListener('click', () => {
     movieView.classList.add('hide');
-    mainMenuDiv.classList.remove('hide');
-})
-
-movieBack.addEventListener('click', e => {
     findMovieDiv.classList.add('hide');
     mainMenuDiv.classList.remove('hide');
-})
+});
 
-addMovieButton.addEventListener('click', e => {
-    mainMenuDiv.classList.add('hide');
-    findAddTitle.innerText = "Add a Movie";
-    findAddbutton.innerText = "Add";
+/* Actor / Director Search */
+const searchCancel = document.getElementById('search-cancel');
+const searchActor = document.getElementById('search-actor');
+const searchDirector = document.getElementById('search-director');
+const search = document.getElementById('search');
+const searchPopup = document.getElementById('search-view');
+
+/* Actor / Director Results */
+const searchResultsDiv = document.getElementById('search-results');
+const searchResults = document.getElementsByClassName('result');
+
+/* Reviews */
+const reviewDivs = Array.from(document.getElementsByClassName('review'));
+const closeReview = document.getElementById('close-review');
+//const saveReview = document.getElementById('save-review');
+
+/**
+ * Function to return from the movie view to the find movie view
+ */
+function returnToFindMovie() {
+    movieView.classList.add('hide');
     findMovieDiv.classList.remove('hide');
-})
+}
 
-edit.addEventListener('click', e => {
+/**
+ * Shows the find movie screen
+ */
+function showFindMovie() {
+    mainMenuDiv.classList.add('hide');
+    findAddTitle.innerText = 'Find a Movie';
+    findAddbutton.innerText = 'Find';
+    findMovieDiv.classList.remove('hide');
+}
+
+/**
+ * Shows the add movie screen
+ */
+function showAddMovie() {
+    mainMenuDiv.classList.add('hide');
+    findAddTitle.innerText = 'Add a Movie';
+    findAddbutton.innerText = 'Add';
+    findMovieDiv.classList.remove('hide');
+}
+
+/**
+ * Enables the editable items to be edited
+ */
+function enableEditableItems() {
     for (let i = 0; i < editableItems.length; i++) {
-        console.log(editableItems[i])
-        editableItems[i].classList.remove('hide')
+        editableItems[i].classList.remove('hide');
     }
     saveButton.classList.remove('hide');
     edit.classList.add('hide');
-})
+}
 
-saveButton.addEventListener('click', e => {
+/**
+ * Disables the editable items
+ */
+function disableEditableItems() {
     for (let i = 0; i < editableItems.length; i++) {
-        console.log(editableItems[i])
-        editableItems[i].classList.add('hide')
+        editableItems[i].classList.add('hide');
     }
-    saveButton.classList.add('hide')
-    edit.classList.remove('hide')
-})
+    saveButton.classList.add('hide');
+    edit.classList.remove('hide');
+}
+
+/**
+ * Hides the shield from the DOM
+ */
+function hideShield() {
+    shield.classList.add('hide');
+}
+
+/**
+ * Throws up the shield overlay
+ */
+function showSheild() {
+    shield.classList.remove('hide');
+}
+
+function hideSearchResults() {
+    searchResultsDiv.classList.add('hide');
+}
+function showSearchResults() {
+    searchResultsDiv.classList.remove('hide');
+}
+function showSearchPopup() {
+    searchPopup.classList.remove('hide');
+}
+function hideSearchPopup() {
+    searchPopup.classList.add('hide');
+}
+
+//TODO: reimplement this
+findAddbutton.addEventListener('click', () => {
+    findMovieDiv.classList.add('hide');
+    movieView.classList.remove('hide');
+});
+
+
+/* Event Listners */
+movieBack.addEventListener('click', () => {
+    returnToFindMovie();
+});
+
+findMovieButton.addEventListener('click', () => {
+    showFindMovie();
+});
+
+addMovieButton.addEventListener('click', () => {
+    showAddMovie();
+});
+
+edit.addEventListener('click', () => {
+    enableEditableItems();
+});
+
+saveButton.addEventListener('click', () => {
+    //TODO: determine what needs to be saved
+    disableEditableItems();
+});
+
+searchCancel.addEventListener('click', () => {
+    hideShield();
+    hideSearchPopup();
+    //TODO: handle hiding the search box as well
+});
+
+searchActor.addEventListener('click', () => {
+    showSheild();
+    showSearchPopup();
+    //TODO: handle for actor
+});
+
+searchDirector.addEventListener('click', () => {
+    showSheild();
+    showSearchPopup();
+    //TODO: handle for director
+});
+
+search.addEventListener('click', () => {
+    hideSearchPopup();
+    showSearchResults();
+});
+
+Array.from(searchResults).forEach((element) => {
+    element.addEventListener('click', () => {
+        hideSearchResults();
+        hideShield();
+    });
+});
+
+const reviewEditor = document.getElementById('review-editor');
+function showViewEditor() {
+    showSheild();
+    //reviewEditor.classList.remove('hide');
+}
+
+function hideViewEditor() {
+    reviewEditor.classList.add('hide');
+    hideShield();
+}
+
+reviewDivs.forEach((element) => {
+    element.addEventListener('click', () => {
+        showViewEditor(element);
+    });
+});
+
+closeReview.addEventListener('click', hideViewEditor);
+saveButton.addEventListener('click', () => {
+    hideViewEditor();
+});
